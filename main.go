637fdd0676b4305e1b2c3db663cbc80fc194c8ba
庄家栋 <jiadong.zhuang@qq.com>
2017-11-07 13:07:53 +0800

    rendering-wrapper

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	ss "strings"
+)
+
+func get_wrapper(c *gin.Context, t string) func(int, interface{}) {
+	switch t {
+	case "XML":
+		return c.XML
+	case "YAML":
+		return c.YAML
+	case "INDENTEDJSON":
+		return c.IndentedJSON
+	default:
+		return c.JSON
+	}
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		t := c.DefaultQuery("type", "JSON")
+		t = ss.ToUpper(t)
+		get_wrapper(c, t)(200, gin.H{
 			"message": "pong",
 		})
+
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
