e4302d8b35e63496b720b9f8ca8d90a589d7392a
庄家栋 <jiadong.zhuang@qq.com>
2018-05-04 20:51:48 +0800

    remove irrelevant-item(s).

--- handler_pingpong.go
+++ handler_pingpong.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"fmt"
 	comm "gin-demo/common"
 	"github.com/gin-gonic/gin"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 )
 
 func pingpongHandler() http.Handler {
 	e := gin.New()
 	e.Use(gin.Recovery())
-	//p := comm.TheProfile()
+	p := comm.TheProfile()
 	e.GET("/ping", func(c *gin.Context) {
-		p := comm.TheProfile() //confirm singleton
+		fmt.Fprintf(os.Stdout, "/ping? in.\n")
 		defer p.Since(time.Now())
 		p.Inc("count", 1)
 		t := c.DefaultQuery("type", "JSON")
+		fmt.Fprintf(os.Stdout, "/ping? done.\n")
 		comm.ObjectWrapper(c, t)(200, gin.H{
 			"message": "pong",
 		})
@@ -24,11 +27,13 @@ func pingpongHandler() http.Handler {
 	})
 
 	e.GET("/slow", func(c *gin.Context) {
-		p := comm.TheProfile() //confirm singleton
+		fmt.Fprintf(os.Stdout, "/slow? in.\n")
 		defer p.Since(time.Now())
 		p.Inc("count", 1)
+		fmt.Fprintf(os.Stdout, "/slow?\n")
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 		t := c.DefaultQuery("type", "JSON")
+		fmt.Fprintf(os.Stdout, "/slow? done.\n")
 		comm.ObjectWrapper(c, t)(200, gin.H{
 			"message": "pong",
 		})
